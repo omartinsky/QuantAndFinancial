45a9c33b9d27783297eb8513093d7b154feaa3ce
Ondrej Martinsky <ondrej.martinsky@gmail.com>
2017-04-30 12:37:29 +0100

    run_all_jupyter_notebooks

--- __run_all_jupyter_notebooks.py
+++ __run_all_jupyter_notebooks.py
@@ -1,32 +1,37 @@
-import sys
+import sys, os
 import nbformat
 from nbconvert.preprocessors import ExecutePreprocessor
 from nbconvert import HTMLExporter
 
-def tohtml(input, output=None):
-	if output is None:
-		output = input + ".html"
-
-	print("Converting %s --> %s" % (input, output))
-	ep = ExecutePreprocessor(timeout=300, kernel_name='python3')
-
-	with open(input) as f:
-		nb = nbformat.read(f, as_version=4)
+def tohtml(working_directory, input, output=None):
+	prevcwd = os.getcwd()
+	try:
+		os.chdir(working_directory)
 		
-	ep.preprocess(nb, {'metadata': {'path': './'}})
+		if output is None:
+			output = input + ".html"
 
-	#filename_output = input + ".executed.ipynb"
-	#with open(filename_output, 'wt') as f:
-	#    nbformat.write(nb, f)
+		print("Converting %s --> %s" % (input, output))
+		ep = ExecutePreprocessor(timeout=300, kernel_name='python3')
 
-	exportHTML = HTMLExporter()
-	(body, resources) = exportHTML.from_notebook_node(nb)
+		with open(input) as f:
+			nb = nbformat.read(f, as_version=4)
+			
+		ep.preprocess(nb, {'metadata': {'path': './'}})
 
-	with open(output, 'wt') as f:
-		f.write(body)
+		#filename_output = input + ".executed.ipynb"
+		#with open(filename_output, 'wt') as f:
+		#    nbformat.write(nb, f)
+
+		exportHTML = HTMLExporter()
+		(body, resources) = exportHTML.from_notebook_node(nb)
+
+		with open(output, 'wt') as f:
+			f.write(body)
+		
+		print("Done")
+	finally:
+		os.chdir(prevcwd)
 	
-	print("Done")
-	
-
-tohtml("binomial_option_pricing/binomial_option_pricing.ipynb")
-tohtml("black_litterman/black_litterman.ipynb")
+tohtml("binomial_option_pricing", "binomial_option_pricing.ipynb")
+tohtml("black_litterman", "black_litterman.ipynb")
